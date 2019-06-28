1bf9286c45038f7c031b39db232f5f2d7abd68ac
福本萌々恵 <hakvt0.89io@gmail.com>
2019-06-28 15:26:56 +0900

    テスト用のマップ描画

--- Prisoner'sRun/Prisoner'sRun/Main/Main.cpp
+++ Prisoner'sRun/Prisoner'sRun/Main/Main.cpp
@@ -1,9 +1,10 @@
 ﻿#include<d3dx9.h>
-#include"Library/Device/Device.h"
-#include"Library/Window/Window.h"
-#include"Library/Input/Keyboard.h"
-#include"Library/Texture/Texture.h"
-#include"Library/Draw/Draw.h"
+#include"../Library/Device/Device.h"
+#include"../Library/Window/Window.h"
+#include"../Library/Input/Keyboard.h"
+#include"../Library/Texture/Texture.h"
+#include"../Library/Draw/Draw.h"
+#include"../Map/Map.h"
 
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int) {
@@ -12,7 +13,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int) {
 	Device::Init(h_wnd);
 	LPDIRECT3DDEVICE9 dev = Device::dev;
 	Key& key = Key::GetInstance();
-	Texture test = "hora_attack.png";
+	//Texture test = "Resource/Object/close_locker.png";
+	Map map;
 
 	while (Window::ProcessMessage()) {
 		
@@ -25,9 +27,11 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int) {
 
 		// テスト用の描画
 		//Draw2D::Box(test, { 100,200 },1,1,0,0,0,0x00ffffff);
+		map.Draw();
 
 		Device::DrawEnd();
 	}
+
 	Device::Release();
 	return 0;
 }
