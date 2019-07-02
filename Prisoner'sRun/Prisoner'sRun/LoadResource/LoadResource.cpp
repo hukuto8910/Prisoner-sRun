9d4e5c0c18d32e65508e1c647b5d415b61e40984
福本萌々恵 <hakvt0.89io@gmail.com>
2019-07-02 13:37:12 +0900

    LoadResourceを名前空間からクラスに変更

---
+++ Prisoner'sRun/Prisoner'sRun/LoadResource/LoadResource.cpp
@@ -0,0 +1,27 @@
+﻿#include"LoadResource.h"
+
+
+void Resource::Load() {
+
+	// タイトル画面の画像登録
+	m_texture_list.emplace(TextureList::TITLE_TEX, "Resource/Texture/Title/title.png");
+
+	// マップの画像登録
+	m_texture_list.emplace(TextureList::MAP_FLOOR1, "Resource/Texture/Map/floor1.png");
+	m_texture_list.emplace(TextureList::MAP_FLOOR2, "Resource/Texture/Map/floor2.png");
+	m_texture_list.emplace(TextureList::MAP_FLOOR3, "Resource/Texture/Map/floor3.png");
+}
+
+void Resource::Relese() {
+
+	for (auto texture_list = m_texture_list.begin(); texture_list != m_texture_list.end();) {
+
+		if (&texture_list != nullptr) {
+			//delete &texture_list;
+			texture_list = m_texture_list.erase(texture_list);
+		}
+		else {
+			texture_list++;
+		}
+	}
+}
--- Prisoner'sRun/Prisoner'sRun/LoadResource/LoadResource.h
+++ Prisoner'sRun/Prisoner'sRun/LoadResource/LoadResource.h
@@ -1,9 +1,7 @@
 ﻿#pragma once
 
-
 #include<unordered_map>
 #include<string>
-#include"../Library/Texture/Texture.h"
 
 
 // 画像のリスト
@@ -16,41 +14,34 @@ enum TextureList {
 	MAP_FLOOR1,
 	MAP_FLOOR2,
 	MAP_FLOOR3,
+
 	TEXTURE_MAX
 };
 
 /*
-・すべての画像をまとめて読み込み保存
-・指定するだけで画像を呼び出せるように
-・enumと合わせる
+・画像名の登録
+
 */
-namespace Resource {
-
-	//Texture m_texture_list[TextureList::TEXTURE_MAX];
-	std::unordered_map<TextureList, Texture>m_texture_list[TextureList::TEXTURE_MAX];
-
-	void Load() {
-		// タイトル画面の画像登録
-		m_texture_list->emplace(TextureList::TITLE_TEX, "Resource/Texture/Title/title.png");
-
-		// マップの画像登録
-		//m_texture_list[TextureList::MAP_FLOOR1] = "Resource/Texture/Map/floor1.png";
-		//m_texture_list[TextureList::MAP_FLOOR2] = "Resource/Texture/Map/floor2.png";
-		//m_texture_list[TextureList::MAP_FLOOR3] = "Resource/Texture/Map/floor3.png";
-		m_texture_list->emplace(TextureList::MAP_FLOOR1, "Resource/Texture/Map/floor1.png");
-		m_texture_list->emplace(TextureList::MAP_FLOOR2, "Resource/Texture/Map/floor2.png");
-		m_texture_list->emplace(TextureList::MAP_FLOOR3, "Resource/Texture/Map/floor3.png");
+class Resource {
+public:
+	// シングルトン
+	static Resource* GetInsutance() {
+		static Resource s_resource;
+		return &s_resource;
 	}
 
-	void Relese() {
-		for (auto texture_list = m_texture_list->begin(); texture_list != m_texture_list->end();) {
-			if (&texture_list != nullptr) {
-				//delete &texture_list;
-				texture_list = m_texture_list->erase(texture_list);
-			}
-			else {
-				texture_list++;
-			}
-		}
+	// 画像名の読み込み関数
+	void Load();
+	// リストの解放関数
+	void Relese();
+
+	std::string GetTextureName(TextureList tex) {
+		auto tex_list = m_texture_list.find(tex);
+		return tex_list->second;
 	}
-}
+
+private:
+	Resource() {}
+	~Resource() {}
+	std::unordered_map<TextureList, std::string> m_texture_list;
+};
