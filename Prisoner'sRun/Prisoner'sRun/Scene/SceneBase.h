5abc802e1413eeed8488e62aaaedc01f3b889236
福本萌々恵 <hakvt0.89io@gmail.com>
2019-07-02 15:22:24 +0900

    基底クラスSceneBaseと管理クラスSceneManagerを追加(ヘッダのみ)

---
+++ Prisoner'sRun/Prisoner'sRun/Scene/SceneBase.h
@@ -0,0 +1,61 @@
+﻿#pragma once
+
+#include"../Library/Input/Keyboard.h"
+
+
+enum SceneId {
+	TITLE,
+	GAME_MAIN,
+	CLEAR_RESULT,
+	GAMEOVER_RESULT,
+	SCENE_QUIT,
+	SCENEID_MAX
+};
+
+class SceneBase {
+public:
+	enum SceneStep {
+		SCENE_INIT,
+		SCENE_UPDATE,
+		SCENE_END
+	};
+
+
+	~SceneBase() {}
+
+	// 初期化～終了処理まで必ず各派生先のシーン内で定義すること
+	virtual void Init() = 0;
+	virtual void Update() = 0;
+	virtual void Draw() = 0;
+	virtual SceneId End() = 0;
+
+	// シーンの順路管理
+	/*
+	基本的に初期化→更新→終了処理の順
+	終了処理以外は自身のシーンを返し、終了処理は次のシーンを返す
+	*/
+	SceneId Control() {
+
+		switch (m_scene_step) {
+		case SceneStep::SCENE_INIT:
+			Init();
+			break;
+		case SceneStep::SCENE_UPDATE:
+			Update();
+			break;
+		case SceneStep::SCENE_END:
+			return End();
+		}
+		
+		return m_scene;
+	}
+
+protected:
+	SceneStep m_scene_step;	// シーン内の順
+	SceneId m_scene;		// 現在のシーン
+
+	Key& key = Key::GetInstance();
+
+private:
+	SceneBase() {}
+};
---
+++ Prisoner'sRun/Prisoner'sRun/Scene/SceneManager.h
@@ -0,0 +1,63 @@
+﻿#pragma once
+
+#include<unordered_map>
+#include"SceneBase.h"
+#include"TitleScene/TitleScene.h"
+
+
+class SceneManager {
+public:
+	// シングルトン
+	static SceneManager& GetInstance() {
+		static SceneManager s_sm;
+		return s_sm;
+	}
+
+
+	void Init() {
+
+		// 各シーンの登録
+		m_scene_list.emplace(SceneId::TITLE, new Title);
+
+	}
+
+	void Update() {
+
+		// 現在のシーンと受け取ったシーンのIDを比較・更新する
+		ChackScene(m_new_scene_id);
+		// 各シーンの現在のステップを実行
+		m_new_scene_id = m_scene->Control();
+	}
+
+	void Draw() {
+		m_scene->Draw();
+	}
+
+	// 現在と新しいシーンIDを比較・更新する　引数にはm_scene_idを入れること
+	void ChackScene(const SceneId new_id) {
+
+		if (m_scene_id == new_id) {
+			return;
+		}
+		// 終了処理が呼ばれた場合はフラグをtrueにする
+		else if (new_id == SceneId::SCENE_QUIT) {
+			m_is_quit = true;
+			return;
+		}
+
+		// 現在のシーンと違う場合、idとシーンを新しいものに更新する
+		m_scene_id = new_id;
+		m_scene = m_scene_list.find(m_scene_id)->second;
+	}
+
+private:
+	SceneManager() {}
+	~SceneManager() {}
+
+	bool m_is_quit;			// ゲームの終了フラグ
+	std::unordered_map<SceneId, SceneBase*> m_scene_list;	// シーン格納リスト
+
+	SceneBase* m_scene;		// 現在のシーン
+	SceneId m_scene_id;		// 現在のシーンのID
+	SceneId m_new_scene_id;	// 新しいシーンのID
+};
