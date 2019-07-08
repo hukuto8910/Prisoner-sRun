d0ecf9a8ff3aa42e8290146b4b8b55f239bc85f2
福本萌々恵 <hakvt0.89io@gmail.com>
2019-07-08 14:05:56 +0900

    マップのファイル分け

---
+++ Prisoner'sRun/Prisoner'sRun/Map/Map.cpp
@@ -0,0 +1,42 @@
+﻿#include"Map.h"
+
+
+int mapChipData[MAP_SIZE_HEIGHT][MAP_SIZE_WIDTH] = {
+	{1,1,1,1,1,1,1,1,1,1},
+	{2,2,2,2,2,2,2,2,2,2},
+	{3,3,3,3,3,3,3,3,3,3}
+};
+
+
+void Map::Draw() {
+
+	for (int i = 0; i < MAP_SIZE_HEIGHT; i++) {
+		for (int j = 0; j < MAP_SIZE_WIDTH; j++) {
+
+			// MapDataのチップ番号を渡す
+			m_mapChip[i][j].id = mapChipData[i][j];
+
+			// 画像のサイズ分ずらして描画する
+			m_mapChip[i][j].pos.x = (float)j * MAP_CHIP_SIZE;
+			m_mapChip[i][j].pos.y = (float)i * MAP_CHIP_SIZE;
+
+			// チップ番号によって描画画像を変える
+			switch (m_mapChip[i][j].id) {
+
+			case FLOOR1:
+				m_texture = res.GetTextureName(TextureList::MAP_FLOOR1);
+				break;
+
+			case FLOOR2:
+				m_texture = res.GetTextureName(TextureList::MAP_FLOOR2);
+				break;
+
+			case FLOOR3:
+				m_texture = res.GetTextureName(TextureList::MAP_FLOOR3);
+				break;
+			}
+
+			Draw2D::Box(m_texture.c_str(), m_mapChip[i][j].pos);
+		}
+	}
+}
--- Prisoner'sRun/Prisoner'sRun/Map/Map.h
+++ Prisoner'sRun/Prisoner'sRun/Map/Map.h
@@ -16,71 +16,23 @@
 ・構造体にするにしても、クラス内で
 ・座標、画像、チップ番号、当たり判定の矩形が必要
 */
-struct MapChip{
-	Texture tex;
-	D3DXVECTOR2 pos;
-	int id;
-};
+
 
 /*
 ・Dataはcsvで読み取るようにする
 ・縦はフル、横はUIを置くので、その分の幅で作成
 ・アイテムはランダム、床などの構造は基本一定
 */
-int mapChipData[MAP_SIZE_HEIGHT][MAP_SIZE_WIDTH] = {
-	{1,1,1,1,1,1,1,1,1,1},
-	{2,2,2,2,2,2,2,2,2,2},
-	{3,3,3,3,3,3,3,3,3,3}
-};
+
 
 class Map {
 public:
+	Map() {}
+	~Map() {};
 
-	Map() {
-
-	}
-	~Map() {
-
-	};
-
-	void Draw() {
-
-		for (int i = 0; i < MAP_SIZE_HEIGHT; i++) {
-			for (int j = 0; j < MAP_SIZE_WIDTH; j++) {
-
-				// MapDataのチップ番号を渡す
-				mapChip[i][j].id = mapChipData[i][j];
-				
-				// 画像のサイズ分ずらして描画する
-				mapChip[i][j].pos.x = (float)j * MAP_CHIP_SIZE;
-				mapChip[i][j].pos.y = (float)i * MAP_CHIP_SIZE;
-
-				// チップ番号によって描画画像を変える
-				switch (mapChip[i][j].id) {
-				case FLOOR1:
-					//test = Resource::m_texture_list.at(TextureList::MAP_FLOOR1);
-					test = res.GetTextureName(TextureList::MAP_FLOOR1);
-					break;
-				case FLOOR2:
-					//test = Resource::m_texture_list.at(TextureList::MAP_FLOOR2);
-					test = res.GetTextureName(TextureList::MAP_FLOOR2);
-
-					break;
-				case FLOOR3:
-					//test = Resource::m_texture_list.at(TextureList::MAP_FLOOR3);
-					test = res.GetTextureName(TextureList::MAP_FLOOR3);
-
-					break;
-				}
-
-				Draw2D::Box(test.c_str(), mapChip[i][j].pos);
-			}
-		}
-	}
+	void Draw();
 
 private:
-	std::string test;
-	MapChip mapChip[MAP_SIZE_HEIGHT][MAP_SIZE_WIDTH];
 	enum MapChipList {
 		MAP_TEXTURE_INIT,
 		FLOOR1,
@@ -89,6 +41,15 @@ private:
 		MAP_TEXTURE_MAX
 	};
 
-	Resource& res=Resource::GetInstance();
+	struct MapChip {
+		Texture tex;
+		D3DXVECTOR2 pos;
+		int id;
+	};
+
+	std::string m_texture;
+	MapChip m_mapChip[MAP_SIZE_HEIGHT][MAP_SIZE_WIDTH];
+	
+	Resource& res = Resource::GetInstance();
 };
 
