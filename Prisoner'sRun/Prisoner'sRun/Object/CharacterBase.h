92e9baa57cb7f0a0450f623f2e98022e9e1afd22
福本萌々恵 <hakvt0.89io@gmail.com>
2019-07-09 23:03:33 +0900

    ObjectBaseの名前変更、CharacterBaseの追加

---
+++ Prisoner'sRun/Prisoner'sRun/Object/CharacterBase.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include"ObjectBase.h"
+
+
+class CharacterBase :public ObjectBase {
+public:
+	CharacterBase() {}
+	virtual ~CharacterBase() {}
+
+	/*
+	・アニメーション回す関数
+	・死亡判定
+	*/
+
+protected:
+	int m_speed;	// 移動速度
+};
