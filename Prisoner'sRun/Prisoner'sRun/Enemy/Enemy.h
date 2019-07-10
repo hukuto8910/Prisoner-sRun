2574ade9c9a4030f1de489f16dd6b1cff3d5ff4c
福本萌々恵 <hakvt0.89io@gmail.com>
2019-07-10 15:53:13 +0900

    敵クラスを追加

---
+++ Prisoner'sRun/Prisoner'sRun/Enemy/Enemy.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include<string>
+#include<d3dx9.h>
+
+
+class Enemy {
+public:
+	Enemy() {}
+	~Enemy() {}
+
+	void Init() {
+
+	}
+
+	void Update() {
+
+	}
+
+	void Draw() {
+
+	}
+
+private:
+	std::string m_texture;
+	D3DXVECTOR2 m_pos;
+	int m_speed;
+};
